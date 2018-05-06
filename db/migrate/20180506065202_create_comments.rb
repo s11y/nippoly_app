d101a424077a9dfd8a3705541950a863b2c2eeda
ShinokiRyosei <shinoki.r.0301@gmail.com>
2018-05-06 15:55:34 +0900

    fix create comments migration

--- db/migrate/20180506065202_create_comments.rb
+++ db/migrate/20180506065202_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
-      t.test :body
+      t.text :body
       t.references :user, foreign_key: true
       t.references :article, foreign_key: true
 
