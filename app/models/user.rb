8b5b378483151676db344157fa006d8f65e55ea4
ShinokiRyosei <shinoki.r.0301@gmail.com>
2018-05-19 02:35:32 +0900

    modified users_controller and views

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   has_many :articles
   has_many :comments
 
-  validates :name, presence: true, length: { maximum: 50 }
+  validates :name, presence: true, length: { maximum: 50 },
+            uniqueness: true
   validates :email, presence: true,
             format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
             uniqueness: { case_sensitive: true }
