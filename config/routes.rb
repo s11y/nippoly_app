3d9a0d70ad0ee4c66d8ff26fbec632e6f706ac2a
ShinokiRyosei <shinoki.r.0301@gmail.com>
2018-05-21 01:12:31 +0900

    chaneg routing

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
 
-  resources :teams, only: [:index, :new, :create]
+  resources :teams, only: [:index, :show, :new, :create] do
+    resources :users, except: [:index, :show]
+  end
 end
