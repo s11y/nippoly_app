9299f1ee7b8b313835559bb8dd256d2e5aa67ffc
ShinokiRyosei <shinoki.r.0301@gmail.com>
2018-05-21 01:12:10 +0900

    create team logic

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,10 +1,35 @@
 class TeamsController < ApplicationController
   def index
+    if params[:team_name]
+    else
+      @team = Team.new
+    end
+  end
+
+  def show
+    # TODO: あとでログインしているユーザーのTeamにする
+    @team = Team.new
   end
 
   def new
+    @team = Team.new
   end
 
   def create
+    @team = Team.new(team_params)
+
+    if @team.save
+      flash[:success] = 'チームを作成しました'
+      redirect_to new_team_user_path(@team)
+    else
+      flash.now[:danger] = 'チームを作成できませんでした'
+      render :new
+    end
+  end
+
+  private
+
+  def team_params
+    params.require(:team).permit(:name)
   end
 end
