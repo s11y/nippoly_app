83306af1d1ef2a3721f60ed62d922c24d3e6fb29
ShinokiRyosei <shinoki.r.0301@gmail.com>
2018-05-22 22:46:39 +0900

    Modified login method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,33 @@
 class SessionsController < ApplicationController
   def new
+    @team = Team.find(params[:team_id])
   end
 
   def create
+    name = params[:session][:name]
+    password = params[:session][:password]
+
+    if login(name, password)
+      flash[:success] = 'ログインしました'
+      redirect_to @user
+    else
+      flash.now[:danger] = 'ログインできませんでした'
+      render 'new'
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def login(name, password)
+    @user = User.find_by(name: name)
+    if @user && @user.authenticate(password)
+      session[:user_id] = @user.id
+      return true
+    else
+      return false
+    end
   end
 end
