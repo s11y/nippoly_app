40f76444d81a0c3f56ecb8846a4dbe738e98b9b3
ShinokiRyosei <shinoki.r.0301@gmail.com>
2018-05-25 00:05:51 +0900

    Add methods in articlescontroller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,27 @@
 class ArticlesController < ApplicationController
+  def index
+    
+  end
+
+  def new
+    @articles = Article.new
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
