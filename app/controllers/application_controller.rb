10b58afc643acb16497fb9327eb9fad78e1ff5eb
ShinokiRyosei <shinoki.r.0301@gmail.com>
2018-06-03 22:09:20 +0900

    Modified required_user_logged_in method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   def required_user_logged_in
     unless logged_in?
-      redirect_to teams_path
+      redirect_to root_url
     end
   end
 end
