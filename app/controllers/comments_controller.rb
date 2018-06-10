fabca695fe68f74544d7be6107fcaab7b4037f48
ShinokiRyosei <shinoki.r.0301@gmail.com>
2018-06-10 16:28:58 +0900

    Fixed #4 コメントを作成できるようにする

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,21 @@
 class CommentsController < ApplicationController
+
+  def create
+    @article = Article.find(params[:article_id])
+    comment = @article.comments.build(comment_params)
+    comment.user = current_user
+
+    if comment.save
+      redirect_to user_article_path(@article)
+    else
+      redirect_to user_article_path(@article)
+    end
+  end
+
+  private
+
+  def comment_params
+
+    params.require(:comment).permit(:body)
+  end
 end
