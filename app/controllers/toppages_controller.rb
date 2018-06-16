fbb5be071feb1d0e6d7ccd05512025936491148d
ShinokiRyosei <shinoki.r.0301@gmail.com>
2018-06-16 22:33:10 +0900

    Fixed #19 ルートページにチームの投稿を一覧表示する

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,7 +1,7 @@
 class ToppagesController < ApplicationController
   def index
     if logged_in?
-      @articles = current_user.articles
+      @articles = current_team.articles
     end
   end
 end
